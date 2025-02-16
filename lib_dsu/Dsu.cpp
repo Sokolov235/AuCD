b32eeca337ef2467396d545f35bb929597a38db5
Кирилл Соколов <mr.kir2004@gmail.com>
2025-02-16 10:44:44 +0300

    added declaration of dsu2

---
+++ lib_dsu/Dsu.cpp
@@ -0,0 +1,5 @@
+// Copyright 2024 Kirill Sokolov
+
+#include <stdexcept>
+#include "../lib_dsu/Dsu.h"
+
---
+++ lib_dsu/Dsu.h
@@ -0,0 +1,20 @@
+// Copyright 2024 Kirill Sokolov
+
+#ifndef LIB_DSU_DSU_H_
+#define LIB_DSU_DSU_H_
+
+class DSU {
+	int _size;
+	int* _parent;
+	int* _rank;
+public:
+	DSU(int size = 0);
+	~DSU();
+	void make_set(int elem);
+	int find(int elem);
+	void join(int first, int second);
+	void clear();
+};
+
+
+#endif  // LIB_DSU_DSU_H_
