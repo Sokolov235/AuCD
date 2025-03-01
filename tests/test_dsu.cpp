94a47216177f1b1045739a48faf56376234567a8
Кирилл Соколов <mr.kir2004@gmail.com>
2025-03-01 19:20:42 +0300

    пишешь что добавлял или изменял

--- tests/test_dsu.cpp
+++ tests/test_dsu.cpp
@@ -1,4 +1,46 @@
 // Copyright 2024 Kirill Sokolov
 
 #include <gtest.h>
-#include "../lib_dsu/dsu.h"
+#include "../lib_DSU/DSU.h"
+
+TEST(DSUTest, MakeSetTest) {
+    DSU dsu(10);
+    for (int i = 0; i < 10; ++i) {
+        dsu.make_set(i);
+        EXPECT_EQ(dsu.find(i), i);
+    }
+}
+
+TEST(DSUTest, UnionSetsTest) {
+    DSU dsu(10);
+    dsu.union_sets(0, 1);
+    EXPECT_EQ(dsu.find(0), dsu.find(1));
+
+    dsu.union_sets(2, 3);
+    EXPECT_EQ(dsu.find(2), dsu.find(3));
+
+    dsu.union_sets(0, 2);
+    EXPECT_EQ(dsu.find(0), dsu.find(3));
+}
+
+TEST(DSUTest, PathCompressionTest) {
+    DSU dsu(10);
+    dsu.union_sets(0, 1);
+    dsu.union_sets(1, 2);
+    dsu.union_sets(2, 3);
+
+    EXPECT_EQ(dsu.find(0), dsu.find(3));
+    EXPECT_EQ(dsu.find(1), dsu.find(3));
+    EXPECT_EQ(dsu.find(2), dsu.find(3));
+}
+
+TEST(DSUTest, ClearTest) {
+    DSU dsu(10);
+    dsu.union_sets(0, 1);
+    dsu.union_sets(2, 3);
+    dsu.clear();
+
+    for (int i = 0; i < 10; ++i) {
+        EXPECT_EQ(dsu.find(i), i);
+    }
+}
